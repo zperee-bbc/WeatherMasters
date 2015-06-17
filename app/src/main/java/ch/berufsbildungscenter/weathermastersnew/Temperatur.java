fc84e994f3694d55059714591f0fed9016ace458
zmartl-bbc <luca.marti@bbcag.ch>
2015-06-17 12:29:02 +0200

    Temperatur :+1: Juhui

---
+++ app/src/main/java/ch/berufsbildungscenter/weathermastersnew/Temperatur.java
@@ -0,0 +1,36 @@
+package ch.berufsbildungscenter.weathermastersnew;
+
+/**
+ * Created by zmartl on 17.06.2015.
+ * Version ${VERSION}
+ */
+public class Temperatur {
+    private double temp;
+    private double maxTemp;
+    private double minTemp;
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public double getTemp() {
+
+        return temp;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+}
