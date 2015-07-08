48641d8019be6330880ec4f730bb9c2e1f4f67af
zmartl <luca.marti@bbcag.ch>
2015-07-08 09:53:52 +0200

    Test One

---
+++ app/src/androidTest/java/ch/berufsbildungscenter/weathermasters/WeatherMastersScenarioTest.java
@@ -0,0 +1,14 @@
+package ch.berufsbildungscenter.weathermasters;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class WeatherMastersScenarioTest extends ApplicationTestCase<Application> {
+    public WeatherMastersScenarioTest() {
+        super(Application.class);
+    }
+
+}
